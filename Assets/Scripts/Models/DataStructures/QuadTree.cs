ca909161ea3ca79b1094450b5129f6ffb5b02e37
Efi Kaltirimidou <efikalti@gmail.com>
2020-01-11 17:07:25 +0200

    Generate dungeon with quadtree

---
+++ Assets/Scripts/Models/DataStructures/QuadTree.cs
@@ -0,0 +1,128 @@
+﻿using Assets.Scripts.Enums;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Models.DataStructures
+{
+    class QuadTree<T> : IEnumerable<QuadTree<T>>
+    {
+
+        public QuadTreeLeafType Type { get; set; }
+        public QuadTree<T> Parent { get; set; }
+        public ICollection<QuadTree<T>> Children { get; set; }
+
+        public BoundsInt LeafBounds;
+
+        public QuadTree(QuadTreeLeafType type, BoundsInt bounds)
+        {
+            Type = type;
+            Children = new List<QuadTree<T>>();
+            LeafBounds = bounds;
+        }
+
+        public QuadTree<T> AddChild(QuadTreeLeafType child, BoundsInt bounds)
+        {
+            QuadTree<T> childNode = new QuadTree<T>(child, bounds) { Parent = this };
+            Children.Add(childNode);
+            return childNode;
+        }
+
+        public IEnumerator<QuadTree<T>> GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return this;
+            foreach (var directChild in Children)
+            {
+                foreach (var anyChild in directChild)
+                    yield return anyChild;
+            }
+        }
+
+        public void Split(double possibility = 1)
+        {
+            // Check if you can split this node into 4 leafs
+            if (LeafBounds.size.x == 1 || LeafBounds.size.y == 1)
+            {
+                return;
+            }
+
+            // Check if this node is not already split
+            if (Children.Count != 0)
+            {
+                return;
+            }
+
+            var randomGenerator = new System.Random();
+            var possibilityToSplit = randomGenerator.NextDouble();
+
+            if (possibilityToSplit < possibility)
+            {
+
+                var topLeftBounds = CalculateTopLeftBounds();
+                AddChild(QuadTreeLeafType.WALL, topLeftBounds);
+
+                var topRightBounds = CalculateTopRightBounds();
+                AddChild(QuadTreeLeafType.WALL, topRightBounds);
+
+                var bottomLeftBounds = CalculateBottomLeftBounds();
+                AddChild(QuadTreeLeafType.WALL, bottomLeftBounds);
+
+                var bottomRightBounds = CalculateBottomRightBounds();
+                AddChild(QuadTreeLeafType.WALL, bottomRightBounds);
+            }
+        }
+
+        public BoundsInt CalculateTopLeftBounds()
+        {
+            BoundsInt bounds = new BoundsInt();
+            {
+                bounds.xMin = LeafBounds.xMin;
+                bounds.xMax = (LeafBounds.xMax - LeafBounds.xMin) / 2;
+                bounds.yMin = 1 + ((LeafBounds.yMax - LeafBounds.yMin) / 2);
+                bounds.yMax = LeafBounds.yMax;
+            }
+            return bounds;
+        }
+
+        public BoundsInt CalculateTopRightBounds()
+        {
+            BoundsInt bounds = new BoundsInt();
+            {
+                bounds.xMin = 1 + ((LeafBounds.xMax - LeafBounds.xMin) / 2);
+                bounds.xMax = LeafBounds.xMax;
+                bounds.yMin = 1 + ((LeafBounds.yMax - LeafBounds.yMin) / 2);
+                bounds.yMax = LeafBounds.yMax;
+            }
+            return bounds;
+        }
+
+        public BoundsInt CalculateBottomLeftBounds()
+        {
+            BoundsInt bounds = new BoundsInt();
+            {
+                bounds.xMin = LeafBounds.xMin;
+                bounds.xMax = (LeafBounds.xMax - LeafBounds.xMin) / 2;
+                bounds.yMin = LeafBounds.yMin;
+                bounds.yMax = (LeafBounds.yMax - LeafBounds.yMin) / 2;
+            }
+            return bounds;
+        }
+
+        public BoundsInt CalculateBottomRightBounds()
+        {
+            BoundsInt bounds = new BoundsInt();
+            {
+                bounds.xMin = 1 + ((LeafBounds.xMax - LeafBounds.xMin) / 2);
+                bounds.xMax = LeafBounds.xMax;
+                bounds.yMin = LeafBounds.yMin;
+                bounds.yMax = (LeafBounds.yMax - LeafBounds.yMin) / 2;
+            }
+            return bounds;
+        }
+    }
+}
---
+++ Assets/Scripts/QuadTreeTilemapGenerator.cs
@@ -0,0 +1,79 @@
+﻿using Assets.Scripts.Controllers.TilemapController;
+using Assets.Scripts.Enums;
+using Assets.Scripts.Models.DataStructures;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Scripts
+{
+    class QuadTreeTilemapGenerator : TilemapControllerBase
+    {
+        private BoundsInt tilemapBounds;
+
+        private readonly int sideSize = 30;
+
+        private QuadTree<QuadTreeLeafType> quadTree;
+
+        private Queue<QuadTree<QuadTreeLeafType>> SplitQueue;
+
+        public void Start()
+        {
+            SetupTilemapGeneration();
+        }
+
+
+        public void SetupTilemapGeneration()
+        {
+            // Create tilemap if it does not exist
+            tilemap = GetComponent<Tilemap>();
+            if (tilemap == null)
+            {
+                tilemap = transform.gameObject.AddComponent<Tilemap>();
+                transform.gameObject.AddComponent<TilemapRenderer>();
+            }
+
+            // Initialize TilemapHelper object
+            tilemapHelper = new TilemapHelper(TilesArray);
+            // Set tilemap bounds object to the value of sideSize x  sideSize x 0
+            tilemapBounds = new BoundsInt(Vector3Int.zero, new Vector3Int(sideSize, sideSize, 0));
+
+            // Initialize QuadTree
+            quadTree = new QuadTree<QuadTreeLeafType>(QuadTreeLeafType.WALL, tilemapBounds);
+
+            // Initialize queue for splitting the tree nodes, and add the root to the queue
+            SplitQueue = new Queue<QuadTree<QuadTreeLeafType>>();
+            SplitQueue.Enqueue(quadTree);
+
+            GenerateLevel();
+        }
+
+
+        public void GenerateLevel()
+        {
+            int count = 0;
+            int maxCount = 50;
+
+            double possibility = 1d;
+
+            while(SplitQueue.Count > 0)
+            {
+                var node = SplitQueue.Dequeue();
+                node.Split(possibility);
+                foreach(var child in node.Children)
+                {
+                    SplitQueue.Enqueue(child);
+                }
+                count++;
+                if (count == maxCount)
+                {
+                    possibility = 0.5;
+                }
+            }
+
+            // Write tilemap to file
+            fileParser.WriteTilemap(tilemap);
+
+        }
+    }
+}
