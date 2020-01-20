214d512aa57504e0362111c20fc6696b22c291e5
Efi Kaltirimidou <efikalti@gmail.com>
2020-01-20 20:55:19 +0200

    Finished evaluation in python

--- GAN/src/evaluate.py
+++ GAN/src/evaluate.py
@@ -10,8 +10,9 @@ class Evaluator:
         self.data = np.ndarray(shape=(DUNGEON_DIMENSION,
                                       DUNGEON_DIMENSION), dtype=int)
         self.rooms = None
+        self.results = None
 
-    def Convert(self, generated_data):
+    def convert(self, generated_data):
         # Generated data are a dungeon_dimension^2 1D array
         # Convert to the 2D  dungeon_dimension x dungeon_dimension array
         index = 0
@@ -19,7 +20,6 @@ class Evaluator:
             for y in range(0, DUNGEON_DIMENSION):
                 self.data[x][y] = generated_data[index]
                 index += 1
-
         print(self.data)
 
     def is_tile_of_type(self, vector, type_id):
@@ -88,11 +88,11 @@ class Evaluator:
             for y in range(bounds.y_min, bounds.y_max):
                 position.y = y
                 if self.is_tile_of_type(position,
-                                        TILE_TYPES["WALL"]) is True:
+                                        TILE_TYPES["WALL"]):
                     # Found wall tile
                     result, next_position, room_bounds = self.is_room(position,
                                                                       bounds)
-                    if result is True:
+                    if result:
                         room_areas.append(room_bounds)
         self.rooms = room_areas
         return room_areas
@@ -102,9 +102,10 @@ class Evaluator:
         current_position = Vector(start_position.x, start_position.y + 1)
 
         # Check vertical for the room bounds
-        while(current_position.y <= area_bounds.y_max and not found_floor):
-            if self.is_tile_of_type(current_position,
-                                    TILE_TYPES["WALL"]) is True:
+        while(current_position.y <= area_bounds.y_max
+              and not found_floor):
+            if not self.is_tile_of_type(current_position,
+                                        TILE_TYPES["WALL"]):
                 found_floor = True
                 current_position.y -= 1
             else:
@@ -112,51 +113,52 @@ class Evaluator:
         room_bounds_y = current_position.y
         next_position = room_bounds_y + 1
 
-        if found_floor is False or room_bounds_y == start_position.y:
-            return False, next_position
+        if not found_floor or room_bounds_y == start_position.y:
+            return False, next_position, None
 
         # Check horizontal for the room bounds
         found_floor = False
         current_position.x += 1
 
-        while(current_position.x <= area_bounds.x_max and not found_floor):
-            if self.is_tile_of_type(current_position,
-                                    TILE_TYPES["WALL"]) is True:
+        while(current_position.x <= area_bounds.x_max
+              and not found_floor):
+            if not self.is_tile_of_type(current_position,
+                                        TILE_TYPES["WALL"]):
                 found_floor = True
                 current_position.x -= 1
             else:
                 current_position.x += 1
 
         room_bounds_x = current_position.x
-        if found_floor is False or room_bounds_x == start_position.x:
-            return False, next_position
+        if not found_floor or room_bounds_x == start_position.x:
+            return False, next_position, None
 
         # Check vertical with the bounds found for y
         for y in range(start_position.y, room_bounds_y + 1):
             current_position.y = y
-            if self.is_tile_of_type(current_position,
-                                    TILE_TYPES["WALL"]) is False:
-                return False, next_position
+            if not self.is_tile_of_type(current_position,
+                                        TILE_TYPES["WALL"]):
+                return False, next_position, None
 
         # Check horizontal with the bounds found for y
         current_position.y = start_position.y
         for x in range(start_position.x, room_bounds_x + 1):
             current_position.x = x
-            if self.is_tile_of_type(current_position,
-                                    TILE_TYPES["WALL"]) is False:
-                return False, next_position
+            if not self.is_tile_of_type(current_position,
+                                        TILE_TYPES["WALL"]):
+                return False, next_position, None
 
         if abs(start_position.x - room_bounds_x) == 1 or \
            abs(start_position.y - room_bounds_y) == 1:
-            return False, next_position
+            return False, next_position, None
 
         room_bounds = Bounds(start_position.x, start_position.y,
                              room_bounds_x, room_bounds_y)
         return True, next_position, room_bounds
 
-    def evaluate_rooms(self):
-        rooms = self.find_rooms()
-
+    def evaluate_rooms(self, bounds):
+        rooms = self.find_rooms(bounds)
+        print(len(rooms))
         score = 0
         if len(rooms) > 3:
             score = 100
@@ -169,5 +171,56 @@ class Evaluator:
         sum = 0
         for room_area in rooms:
             sum += self.evaluate_tiles_in_bounds_are_of_type(
-                room_area, TILE_TYPES["WALLS"])
+                room_area, TILE_TYPES["ROOM"])
         return sum
+
+    def evaluate_dungeon(self, data):
+        self.convert(data)
+
+        results = list()
+
+        bounds = Bounds(0, 0, DUNGEON_DIMENSION, DUNGEON_DIMENSION)
+
+        # Evaluate bounds of tilemap are walls
+        bounds_are_walls_result = self.evaluate_bounds_are_of_type(
+            bounds,
+            TILE_TYPES["WALL"])
+
+        results.append(bounds_are_walls_result)
+        print("bounds_are_walls_result: " + str(bounds_are_walls_result))
+
+        # Evaluate cells next to bounds of tilemap are corridors
+        corridor_bounds = Bounds(bounds.x_min + 1, bounds.y_min + 1,
+                                 bounds.x_max - 1, bounds.y_max - 1)
+        next_to_bounds_are_corridors_result = self.evaluate_bounds_are_of_type(
+            corridor_bounds,
+            TILE_TYPES["CORRIDOR"])
+
+        results.append(next_to_bounds_are_corridors_result)
+        print("next_to_bounds_are_corridors_result: "
+              + str(next_to_bounds_are_corridors_result))
+
+        # Evaluate if there are any rooms in the dungeon
+        room_bounds = Bounds(bounds.x_min + 2, bounds.y_min + 2,
+                             bounds.x_max - 2, bounds.y_max - 2)
+        number_of_rooms_result = self.evaluate_rooms(room_bounds)
+
+        results.append(number_of_rooms_result)
+        print("number_of_rooms_result: "
+              + str(number_of_rooms_result))
+
+        # Evaluate the cells in room areas
+        room_area_result = self.evaluate_room_areas(self.rooms)
+        results.append(room_area_result)
+
+        print("room_area_result: "
+              + str(room_area_result))
+
+        self.total_score(results)
+
+    def total_score(self, results):
+        sum = 0
+        for result in results:
+            sum += result
+
+        print("Total room score: " + str(sum))
--- GAN/src/run.py
+++ GAN/src/run.py
@@ -7,13 +7,11 @@ def main():
     file_parser = FileParser()
     data = file_parser.get_csv_data()
 
-    print(data[0])
-
     #gan = GAN()
     #gan.train(data, epochs=100000, batch_size=48, sample_interval=10000)
 
     evaluator = Evaluator()
-    evaluator.Convert(data[0])
+    evaluator.evaluate_dungeon(data[0])
 
 
 if __name__ == "__main__":
