44a45a41cbd7b2ef064c4e6676c55a1344934a1d
Efi Kaltirimidou <efikalti@gmail.com>
2020-02-11 20:19:08 +0200

    separate ui manager for generator

---
+++ Assets/Scripts/UI/UIGeneratorManager.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.Controllers.TilemapController;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGeneratorManager : MonoBehaviour
+{
+    private Button StartButton;
+
+    private TilemapControllerBase TilemapGenerator;
+
+    public void Start()
+    {
+        var tilemapControllers = FindObjectsOfType<TilemapControllerBase>();
+        if (tilemapControllers.Length > 0)
+        {
+            TilemapGenerator = tilemapControllers[0];
+        }
+        StartButton = GetComponent<Button>();
+        StartButton.onClick.AddListener(StartGeneratingOnClick);
+    }
+
+    void StartGeneratingOnClick()
+    {
+        if (TilemapGenerator != null)
+        {
+            TilemapGenerator.StartGenerating();
+        }
+    }
+}
